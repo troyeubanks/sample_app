74f6cd89cfbea255583fd8bbf950c27092e0e41b
Troy Eubanks <TEubanks01@gmail.com>
2015-07-18 20:55:57 -0400

    add basic tests for signup page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+	def setup
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
   test "should get new" do
     get :new
     assert_response :success
   end
-
 end
