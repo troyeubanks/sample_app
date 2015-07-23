63f71681bc4c06828b951c4dd013cf98458ec4d7
Troy Eubanks <TEubanks01@gmail.com>
2015-07-22 22:00:13 -0400

    created user model and basic validation tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+
+	def setup
+		@user = User.new(name: "Test Modaniston", email: "email@email.com")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name = '    '
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = '        '
+		assert_not @user.valid?
+	end
+
+	test "name should not be too long" do
+		@user.name = "A" * 51
+		assert_not @user.valid?
+	end
+
+	test "email should not be too long" do
+		@user.email = "a" * 244 + "@example.com"
+		assert_not @user.valid?
+	end
+end
