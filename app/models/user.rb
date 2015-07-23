f01b6056f7cea5c7d467bafc148931cd01579b13
Troy Eubanks <TEubanks01@gmail.com>
2015-07-22 23:00:15 -0400

    fix migration problem and migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
 	VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },
 							format: { with: VALID_EMAIL_REGEX },
 							uniqueness: { case_sensitive: false }
 
-
+	has_secure_password
 end
