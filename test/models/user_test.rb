57f7c35a61d152f0be954c8ed0e9b3405a008e66
Troy Eubanks <TEubanks01@gmail.com>
2015-07-26 14:05:06 -0400

    setup basic user profile page

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,4 +67,11 @@ class UserTest < ActiveSupport::TestCase
 		@user.password = @user.password_confirmation = 'a' * 5
 		assert_not @user.valid?
 	end
+
+	test "email addresses should be saved as downcase" do
+		mixed_case_email = "NotDoWnCASE@examPLE.coM"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
 end
