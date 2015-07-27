3e89e4a446486c8c5cf8f78660e542c91cb7c181
Troy Eubanks <TEubanks01@gmail.com>
2015-07-26 22:26:42 -0400

    add flash to successful signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
 
   	if @user.save
-  		redirect_to user_path(@user)
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
   	else
   		render :new
   	end
@@ -21,7 +22,8 @@ class UsersController < ApplicationController
   private
 
 	  def user_params
-	  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	  	params.require(:user).permit(:name, :email, :password, 
+	  																:password_confirmation)
 	  end
 
 end
