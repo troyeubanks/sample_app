7553be7a005f3f16fa90ba6c283b32ade7b31cc5
Troy Eubanks <TEubanks01@gmail.com>
2015-07-31 20:53:11 -0400

    add admin bool to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name: "Example User",
 							email: "test@example.com",
 							password: 						 "something",
-							password_confirmation: "something")
+							password_confirmation: "something",
+							admin: true)
 
 99.times do |n|
 	name = Faker::Name.name
