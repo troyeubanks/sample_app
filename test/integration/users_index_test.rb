7f3f2f087d9c0a5cae2bb2bfd37252bb2cdc6fba
Troy Eubanks <TEubanks01@gmail.com>
2015-07-31 21:53:52 -0400

    finish user destroy action and fix index test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,8 +11,8 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 		get users_path
 		assert_template 'users/index'
 		assert_select 'ul.pagination'
-		#User.paginate(page: 1).each do |user|
-		#	assert_select 'a[href=?]', user_path(user), text: user.name
-		#end
+		User.paginate(page: 1, per_page: 20).each do |user|
+			assert_select 'a[href=?]', user_path(user), text: user.name
+		end
 	end
 end
