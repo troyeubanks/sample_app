948e21956d9ca10db4825d8825ccc578e107b0b1
Troy Eubanks <TEubanks01@gmail.com>
2015-08-22 17:28:00 -0400

    update gemfile and replace gemfile.lock

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module SampleApp
     config.active_record.raise_in_transactional_callbacks = true
 
     # Include the authenticity token in remote forms.
-    config.action_view.embed_authenticity_token_in_remote_forms = true
+    #config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
